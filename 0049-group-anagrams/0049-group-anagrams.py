50e83f33ed83b11b9f1b46ada408546d91f15d67
Denize <41974125+dignacio0815@users.noreply.github.com>
2023-06-20 23:01:57 -0700

    Time: 106 ms (89.04%), Space: 20.3 MB (50.83%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,6 +1,6 @@
 '''
-time: O(n) because algo has to loop over entire list of string and add to hashMap
-space: O(n) because 
+time: O(n log n) because algo has to loop over entire list of string and add to hashMap but also sorts the entire string when comparing
+space: O(n) because algo stores n number of unique anagrams as keys in hashMap
 '''
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
