2b5c32c00a5d05db426277de995d3074d864a7e5
Denize <41974125+dignacio0815@users.noreply.github.com>
2023-07-11 20:37:22 -0700

    Time: 47 ms (52.39%), Space: 16.3 MB (20.74%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.py
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.py
@@ -3,12 +3,20 @@ P -> single int
 r -> return back smallest multiple as an int
 e -> given n = 5, S.E.M. is 10 
 p -> 
+
+Optimal solution:
+Time: O(1)
+Space: O(1)
+
+Trivial solution:
+Time: O(n)
+Space: O(1) considering that the flag and num vars are constants
 '''
 class Solution:
     def smallestEvenMultiple(self, n: int) -> int:
         return self.optimalSolution(n)
         
-    def bruteForce(self, n: int) -> int:
+    def trivialSolution(self, n: int) -> int:
         flag = True
         num = 1
         while flag:
