d8697742e89b2ee921404fc00b3866d843f59c52
Denize <41974125+dignacio0815@users.noreply.github.com>
2023-07-13 19:45:56 -0700

    Time: 51 ms (34.20%), Space: 16.2 MB (70.26%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,7 +1,8 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         # return self.solutionOne(jewels, stones)
-        return self.solutionTwo(jewels, stones)
+        # return self.solutionTwo(jewels, stones)
+        return self.solutionThree(jewels, stones)
         
     def solutionOne(self, jewels: str, stones: str) -> int:
         jewelCount = 0
@@ -25,3 +26,13 @@ class Solution:
                 jewelMap[s] += 1
         
         return sum(jewelMap.values())
+    
+    def solutionThree(self, jewels: str, stones: str) -> int:
+        jewelSet = set(jewels)
+        jewelCount = 0
+        
+        for s in stones:
+            if s in jewelSet:
+                jewelCount += 1
+                
+        return jewelCount
