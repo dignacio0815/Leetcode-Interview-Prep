9fdbe365db5e6b50df2be11bef99393f935754af
Denize <41974125+dignacio0815@users.noreply.github.com>
2023-07-13 19:43:20 -0700

    Time: 51 ms (34.38%), Space: 16.3 MB (70.26%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        # return self.solutionOne(jewels, stones)
+        return self.solutionTwo(jewels, stones)
+        
+    def solutionOne(self, jewels: str, stones: str) -> int:
+        jewelCount = 0
+        
+        for j in jewels:
+            for s in stones:
+                if j == s:
+                    jewelCount += 1
+                    
+        return jewelCount
+    
+    def solutionTwo(self, jewels: str, stones: str) -> int:
+        jewelMap = {}
+        
+        for j in jewels:
+            # no need to check for duplicate keys since jewels are UNIQUE
+            jewelMap[j] = 0
+            
+        for s in stones:
+            if s in jewelMap:
+                jewelMap[s] += 1
+        
+        return sum(jewelMap.values())
