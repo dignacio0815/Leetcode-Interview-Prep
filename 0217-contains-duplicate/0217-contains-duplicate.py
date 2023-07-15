d1216001d33232ffaafaeb57fd7e38d3e9fa3362
Denize <41974125+dignacio0815@users.noreply.github.com>
2023-07-15 13:11:48 -0700

    Time: 536 ms (88.82%), Space: 31.9 MB (25.73%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -3,12 +3,8 @@ resources: https://www.geeksforgeeks.org/set-add-python/
 '''
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return self.solutionOne(nums)
-        
-    def solutionOne(self, nums: List[int]) -> bool:
         dupsSet = set()
         for num in nums:
-            print(num)
             if num in dupsSet:
                 return True
             dupsSet.add(num)
