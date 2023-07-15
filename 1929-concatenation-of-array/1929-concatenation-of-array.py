604e149ecd51746d0b68130715ee1877f27993da
Denize <41974125+dignacio0815@users.noreply.github.com>
2023-07-14 19:03:30 -0700

    Time: 88 ms (95.07%), Space: 16.6 MB (85.76%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return self.solutionOne(nums)
+    
+    def solutionOne(self, nums: List[int]) -> List[int]:
+        return nums + nums
+    
+    def solutionTwo(self, nums: List[int]) -> List[int]:
+        pass
+        
