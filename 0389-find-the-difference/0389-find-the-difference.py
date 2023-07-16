155aa30d6eb999708fa3addd224cc2f0d4e74e54
Denize <41974125+dignacio0815@users.noreply.github.com>
2023-07-16 13:19:40 -0700

    Time: 47 ms (76.69%), Space: 16.2 MB (88.84%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,10 +1,14 @@
+'''
+resource links:
+- https://www.programiz.com/python-programming/methods/built-in/enumerate
+'''
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         s = ''.join(sorted(s))
         t = ''.join(sorted(t))
         
-        for idx in range(len(s)):
+        for idx, letter in enumerate(s):
             if (s[idx] != t[idx]):
                 return t[idx]
         
-        return t[-1]
+        return t[-1]        
