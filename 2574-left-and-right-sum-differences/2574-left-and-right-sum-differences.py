842e97a87dc5ba67959be9f625994600b15d5e44
Denize <41974125+dignacio0815@users.noreply.github.com>
2023-07-18 22:46:04 -0700

    Time: 87 ms (77.83%), Space: 16.5 MB (84.91%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
@@ -3,22 +3,29 @@ class Solution:
         return self.runningSum(nums)
     
     def runningSum(self, nums: List[int]) -> List[int]:
+        # init arrays size of nums to 0
         left, right = [0] * len(nums), [0] * len(nums)
+        # total to keep track of running sum in left and right arrays
         total = 0
         
+        # calculate running sum from left to right
         for i in range(1, len(nums)):
             total += nums[i-1]
             left[i] = total
         
-        # reset counter to 0
+        # reset total to 0 since we're looping over right to left now 
         total = 0
         
+        # calculate running sum from right to left
         for i in reversed(range(len(nums))):
+            # if index is 0, we can break out of loop
             if (i == 0):
                 break
             total += nums[i]
             right[i - 1] = total
         
+        # find abs val of left - right 
+        # note either right or left array could've been used here
         for i, val in enumerate(left):
             left[i] = abs(val - right[i])
                 
