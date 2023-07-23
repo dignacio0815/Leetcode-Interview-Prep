bf57972270902aaecec1f1e0c6ef3717764d33eb
Denize <41974125+dignacio0815@users.noreply.github.com>
2023-07-22 23:30:38 -0700

    Time: 47 ms (49.63%), Space: 16.4 MB (19.48%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
@@ -1,5 +1,8 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
+        return self.solutionTwo(address)
+
+    def solutionOne(self, address: str) -> str:
         defangedAddress = ""
         for i, val in enumerate(address):
             if (val == '.'):
@@ -7,3 +10,6 @@ class Solution:
             else:
                 defangedAddress += val
         return defangedAddress
+    
+    def solutionTwo(self, address: str) -> str:
+        return "[.]".join(address.split("."))
