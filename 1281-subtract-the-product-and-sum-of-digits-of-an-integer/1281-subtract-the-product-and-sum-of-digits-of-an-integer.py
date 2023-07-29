b1a88824805164d1b1ad676f2137d8247f24643f
Denize <41974125+dignacio0815@users.noreply.github.com>
2023-07-28 19:16:10 -0700

    Time: 26 ms (99.95%), Space: 16.4 MB (25.34%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.py
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.py
@@ -1,8 +1,26 @@
 class Solution:
     def subtractProductAndSum(self, n: int) -> int:
+        return self.solutionOne(n)
+
+    def yieldDigit(self, num: int) -> int: 
+        while (num > 0):
+            digit = num % 10
+            yield digit
+            num = num // 10
+            
+    def solutionOne(self, num: int) -> int:
         product = 1
         sum = 0
         
+        for digit in self.yieldDigit(num):
+            product *= digit
+            sum += digit
+            
+        return product - sum
+        
+    def solutionTwo(self, num: int) -> int:
+        product = 1
+        sum = 0
         while n > 0:
             temp = n % 10
             product *= temp
