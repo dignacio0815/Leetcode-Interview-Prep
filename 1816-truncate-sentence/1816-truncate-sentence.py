a1ba8a75120a493ea227c94ce7ee853dfbe32391
Denize <41974125+dignacio0815@users.noreply.github.com>
2023-08-02 19:12:52 -0700

    Time: 37 ms (91.96%), Space: 16.4 MB (40.53%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.py
+++ 1816-truncate-sentence/1816-truncate-sentence.py
@@ -1,10 +1,20 @@
 class Solution:
     def truncateSentence(self, s: str, k: int) -> str:
+        return self.optimal(s, k)
         return self.bruteForce(s, k)
     
     def bruteForce(self, s: str, k: int) -> str:
         splitStr = s.split(" ")
         return (" ").join(splitStr[0:k])
+
+    def optimal(self, s: str, k: int) -> str:
+        for i, l in enumerate(s):
+            if (k == 0):
+                return s[0:i - 1]
+            if (l == ' '):
+                k -= 1
+            
+        return s
             
 
         
