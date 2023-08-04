9af2a0e00ae00a368c811b453ee68b197e748168
Denize <41974125+dignacio0815@users.noreply.github.com>
2023-08-04 16:10:08 -0700

    Time: 23 ms (99.92%), Space: 16.5 MB (17.27%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
@@ -1,6 +1,7 @@
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
-        return self.usingStrings(word1, word2)
+        return self.usingJoin(word1, word2)
+        # return self.usingStrings(word1, word2)
     
     def usingStrings(self, word1: List[str], word2: List[str]) -> bool:
         str1, str2 = "", ""
@@ -12,3 +13,6 @@ class Solution:
             str2 += word2[i]
             
         return str1 == str2
+    
+    def usingJoin(self, word1: List[str], word2: List[str]) -> bool:
+        return ("").join(word1) == ("").join(word2)
