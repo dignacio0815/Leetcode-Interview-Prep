4de11f359c52ffd450576e56a595eaea62bc1c69
Denize <41974125+dignacio0815@users.noreply.github.com>
2023-08-04 19:27:08 -0700

    Time: 36 ms (96.66%), Space: 16.2 MB (67.83%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -4,11 +4,22 @@ class Solution:
         
     def bruteForce(self, nums: List[int]) -> int:
         pairs = 0
-        
         for j in range(len(nums)):
             for i in range(j):
                 if (nums[i] == nums[j]):
-                    pairs += 1
+                    pairs+=1
+        return pairs
         
+    def usingMap(self, nums: List[int]) -> int:
+        pairs = 0
+        
+        freq = {}
+        
+        for n in nums:
+            if (n in freq):
+                pairs += freq[n]
+                freq[n] += 1
+            else:
+                freq[n] = 1                                                                                                                                         
         return pairs
     
