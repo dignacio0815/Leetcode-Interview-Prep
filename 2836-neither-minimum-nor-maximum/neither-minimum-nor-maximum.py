83dad40b866bd49637132c44f6c28f8626885454
Denize <41974125+dignacio0815@users.noreply.github.com>
2023-08-20 13:22:55 -0700

    Time: 346 ms (85.95%) | Memory: 16.4 MB (5.63%) - LeetSync

--- 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.py
+++ 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.py
@@ -1,20 +1,22 @@
 class Solution:
     def findNonMinOrMax(self, nums: List[int]) -> int:
+        return self.solutionUsingFilter(nums)
         minVal = min(nums)
         maxVal = max(nums)
         temp = minVal
         
-#         for n in nums:
-#             if n < maxVal and n > minVal:
-#                 temp = n
-#         if minVal == temp or maxVal == temp:
-#             return -1
-#         return temp
+        for n in nums:
+            if n < maxVal and n > minVal:
+                temp = n
+        if minVal == temp or maxVal == temp:
+            return -1
+        return temp
     
         
         
-#         l = filter(lambda x: (x != minVal and x != maxVal), nums)
-#         print(list(l))        
+    def solutionUsingFilter(self, nums):
+        minVal = min(nums)
+        maxVal = max(nums)
 
         for x in filter(lambda x: x != minVal and x != maxVal, nums):
             return x
