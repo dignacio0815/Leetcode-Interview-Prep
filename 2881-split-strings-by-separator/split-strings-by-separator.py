42b48544a165731dca6df76b257b3a9b347c95b6
Denize <41974125+dignacio0815@users.noreply.github.com>
2023-08-21 18:33:23 -0700

    Time: 123 ms (33.33%) | Memory: 16.4 MB (53.88%) - LeetSync

--- 2881-split-strings-by-separator/split-strings-by-separator.py
+++ 2881-split-strings-by-separator/split-strings-by-separator.py
@@ -1,14 +1,7 @@
 class Solution:
     def splitWordsBySeparator(self, words: List[str], separator: str) -> List[str]:
         return self.withoutUsingSplit(words, separator)
-        arr = []
-        for w in words:            
-            # append is O(1) vs extend which O(n)
-            # decided to use extend since I'd have to loop over the split array either way
-            arr.extend(w.split(separator))
-            
-        return arr
-    
+     
     def withoutUsingSplit(self, words: List[str], separator: str) -> List[str]:
         arr = []
         for w in words:
