cd63a872d15ae0de7639b997cfeae65dccc49ab5
Denize <41974125+dignacio0815@users.noreply.github.com>
2023-08-22 20:26:54 -0700

    Time: 50 ms (94.75%) | Memory: 16.2 MB (96.03%) - LeetSync

--- 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.py
+++ 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.py
@@ -5,13 +5,25 @@ notes:
 '''
 class Solution:
     def maximumNumberOfStringPairs(self, words: List[str]) -> int:
-        return self.nestedForLoopSolution(words)
+        return self.solutionWithSet(words)
 
-    def nestedForLoopSolution(self, words: List[str]) -> int:
+    def solutionWithNestForLoop(self, words: List[str]) -> int:
         numPairs = 0
         for i in range(len(words)):
             for j in range(i + 1, len(words)):
                 if words[i] == words[j][::-1]:
                     numPairs += 1
                     break
+        return numPairs
+    
+    def solutionWithSet(self, words: List[str]) -> int:
+        pairsSet = set()
+        numPairs = 0
+
+        for w in words:
+            if w[::-1] in pairsSet:
+                numPairs += 1
+            else:
+                pairsSet.add(w)
+        
         return numPairs
