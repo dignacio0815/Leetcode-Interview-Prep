8a4ff84d3d89a839869c260ffe0b382a8c518cf4
Denize <41974125+dignacio0815@users.noreply.github.com>
2023-08-26 16:30:09 -0700

    Time: 35 ms (86.34%) | Memory: 16.3 MB (18.37%) - LeetSync

--- 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.py
+++ 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.py
@@ -9,22 +9,41 @@ class Solution:
         return count
 
     def getDigits(self, num: int) -> int:
+        # assume num is 3248
+        # log_10(1) = 0
+        # log_10(10) = 1
+        # log_10(100) = 2
+        # log_10(1000) = 3
+        # log_10(3248) = 3.?
+        # log_10(10000) = 4
+
+        power = 10**int(math.log10(num))
         temp = num
-        # return [int(d) for d in str(num)]
-        while temp > 0:
-            # get right most digit
-            digit = temp % 10
-            
+        while power > 0:
+            digit = temp // power
             yield digit
 
-            # interger divide num
-            temp //= 10
+            temp %= power
+            power //= 10
+
+
+
+        # return [int(d) for d in str(num)]
+        # while temp > 0:
+        #     # get right most digit
+        #     digit = temp % 10
+        #     print("yielding digit ", digit)
+        #     yield digit
+        #     print("after yielding digit ", digit)
+        #     # interger divide num
+        #     temp //= 10
 
 
 
     def getCount(self, num: int) -> int:
         count = 0
         for d in self.getDigits(num):
+            print("using digit ", d)
             if (num % d == 0):
                 count += 1
 
