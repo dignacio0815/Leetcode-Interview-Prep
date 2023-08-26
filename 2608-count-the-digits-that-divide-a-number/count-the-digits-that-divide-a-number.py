429abb23d7a658642bc98915743a005be6aee68d
Denize <41974125+dignacio0815@users.noreply.github.com>
2023-08-26 16:09:56 -0700

    Time: 40 ms (63.46%) | Memory: 16.4 MB (18.37%) - LeetSync

--- 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.py
+++ 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.py
@@ -1,13 +1,13 @@
 class Solution:
     def countDigits(self, num: int) -> int:
-        return self.extractingDigits(num)
+        return self.getCount(num)
+
+        # return self.extractingDigits(num)
 
     def extractingDigits(self, num: int) -> int:
         count = 0
         temp = num
         while temp > 0:
-            # print(num)
-
             # get right most digit
             digit = temp % 10
 
@@ -20,4 +20,15 @@ class Solution:
             # interger divide num
             temp //= 10
             
+        return count
+
+    def getDigits(self, num: int) -> int:
+        return [int(d) for d in str(num)]
+
+    def getCount(self, num: int) -> int:
+        count = 0
+        for d in self.getDigits(num):
+            if (num % d == 0):
+                count += 1
+
         return count
