6a824dcf0f84052258416ab51e1ccf829d012148
Denize <41974125+dignacio0815@users.noreply.github.com>
2023-08-26 15:37:52 -0700

    Time: 58 ms (68.68%) | Memory: 17.8 MB (7.83%) - LeetSync

--- 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.py
+++ 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.py
@@ -6,17 +6,9 @@
 #         self.right = right
 class Solution:
     def evaluateTree(self, root: Optional[TreeNode]) -> bool:
-        return self.shortCiruit(root)
-        # base cases lines 10 - 11
-        # if root is None or 0 <= root.val <= 1:
-        #     return root is not None and root.val == 1
-        
-        # # compute child results
-        # leftResult = self.evaluateTree(root.left)
-        # rightResult = self.evaluateTree(root.right)
+        return self.recursion(root)
+        # return self.shortCiruit(root)
 
-        # # assemble child results for our current node
-        # return (leftResult and rightResult) if root.val == 3 else (leftResult or rightResult)
 
     def shortCiruit(self, root: Optional[TreeNode]) -> bool:
         if root is None or (0 <= root.val <= 1):
@@ -28,3 +20,15 @@ class Solution:
             return leftResult
 
         return self.shortCiruit(root.right)
+
+    def recursion(self, root: Optional[TreeNode]) -> bool:
+        # base cases lines 10 - 11
+        if root is None or 0 <= root.val <= 1:
+            return root is not None and root.val == 1
+        
+        # compute child results
+        leftResult = self.evaluateTree(root.left)
+        rightResult = self.evaluateTree(root.right)
+
+        # assemble child results for our current node
+        return (leftResult and rightResult) if root.val == 3 else (leftResult or rightResult)
