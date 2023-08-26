fa47bdf4228b6f2dd6188c3a266ada7c0b50f35c
Denize <41974125+dignacio0815@users.noreply.github.com>
2023-08-26 16:13:36 -0700

    Time: 32 ms (93.77%) | Memory: 16.2 MB (59.62%) - LeetSync

--- 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.py
+++ 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.py
@@ -5,25 +5,25 @@ class Solution:
         # return self.extractingDigits(num)
 
     def extractingDigits(self, num: int) -> int:
-        count = 0
-        temp = num
-        while temp > 0:
-            # get right most digit
-            digit = temp % 10
-
-            # print(digit)
-            
-            # check num mod digit equals to 0 -> increment count
-            if num % digit == 0:
-                count += 1
-
-            # interger divide num
-            temp //= 10
             
         return count
 
     def getDigits(self, num: int) -> int:
-        return [int(d) for d in str(num)]
+        digits = []
+        temp = num
+        # return [int(d) for d in str(num)]
+        while temp > 0:
+            # get right most digit
+            digit = temp % 10
+            
+            digits.append(digit)
+
+            # interger divide num
+            temp //= 10
+        
+        return digits
+
+
 
     def getCount(self, num: int) -> int:
         count = 0
