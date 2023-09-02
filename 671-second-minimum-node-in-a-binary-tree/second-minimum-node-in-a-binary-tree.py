da11808a59f46bed1225d59d0895129755eeff9a
Denize <41974125+dignacio0815@users.noreply.github.com>
2023-09-01 19:48:43 -0700

    Time: 44 ms (35.21%) | Memory: 16.3 MB (33.91%) - LeetSync

--- 671-second-minimum-node-in-a-binary-tree/second-minimum-node-in-a-binary-tree.py
+++ 671-second-minimum-node-in-a-binary-tree/second-minimum-node-in-a-binary-tree.py
@@ -6,23 +6,23 @@
 #         self.right = right
 class Solution:
     def findSecondMinimumValue(self, root: Optional[TreeNode]) -> int:
-        twoSmallestVals = self.findTwoSmallestVals(root)
-        if len(twoSmallestVals) < 2:
+        allVals = list(set(self.findAllValues(root)))
+        if len(allVals) < 2:
             return -1
-        return twoSmallestVals[1]
+        allVals.sort()
+        return allVals[1]
 
-
-    def findTwoSmallestVals(self, root: Optional[TreeNode]):
+    def findAllValues(self, root: Optional[TreeNode]):
         if not root:
             return []
         # return 2 smallest vals
 
-        leftResult = self.findTwoSmallestVals(root.left)
-        rightResult = self.findTwoSmallestVals(root.right)
+        leftResult = self.findAllValues(root.left)
+        rightResult = self.findAllValues(root.right)
 
         # everything from left and right result
-        candidates = leftResult + rightResult + [root.val]
-        candidates = list(set(candidates))
-        candidates.sort()
+        return leftResult + rightResult + [root.val]
+        # candidates = list(set(candidates))
+        # candidates.sort()
 
-        return candidates[0:2]
+        # return candidates[0:2]
