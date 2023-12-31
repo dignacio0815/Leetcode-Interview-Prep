5532fef8475050d02d3dd5bb2b01d6589aae1bce
Denize <41974125+dignacio0815@users.noreply.github.com>
2023-12-30 20:24:54 -0800

    Time: 761 ms (24.63%) | Memory: 50.3 MB (44.03%) - LeetSync

--- ArraysAndHashing/Easy/448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.kt
+++ ArraysAndHashing/Easy/448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.kt
@@ -4,21 +4,15 @@ class Solution {
         val output = mutableListOf<Int>()
 
         nums.forEach {
-            // println(it)
-            // println(it - 1)
-            // println()
             list[it - 1] = list[it - 1] + 1 ?: 1
         }
-
-        println(list)
 
         list.forEachIndexed { index, `val` ->
             if (`val` == 0) {
                 output.add(index + 1)   
             }
         }
-        
-        println(output)
+
         return output
     }
 }
