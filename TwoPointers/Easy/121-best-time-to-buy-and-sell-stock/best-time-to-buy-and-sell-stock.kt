46fa062813b0deb9dc7795ce15135b54e0192aa8
Denize <41974125+dignacio0815@users.noreply.github.com>
2023-12-30 19:56:02 -0800

    Time: 1054 ms (5.00%) | Memory: 56.4 MB (31.03%) - LeetSync

--- TwoPointers/Easy/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.kt
+++ TwoPointers/Easy/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.kt
@@ -13,59 +13,5 @@ class Solution {
         }
         
         return profit
-
-
-
-
-
-
-
-
-
-
-
-
-        /*
-        // outer loop i = 0 until prices.size - 1
-        //      buy = -prices[i]
-        //      nested loop j = i + 1 ..
-        //          sell = prices[j]
-        //          profit = buy + sell
-        //          if (profit < negative) {}
-        //          elif (profit > positive) {
-                        finalProfit = profit
-                    }
-         */
-        // loop over prices i = 1
-        /*
-            if (sellingPrice - buyingPrice > 0) {
-                finalProfit = max(sellingPrice - buyingPrice, finalProfit)
-            }
-            if (prices[i] < buyingPrice) {
-                buyingPrice = min(buyingPrice, prices[i])
-            } else if (prices[i] > buyingPrice) {
-                sellingPrice = max(sellingPrice, prices[i])
-            }
-
-        return profit
-         */
-
-    
-        
-
-
-        //  for (i in 0 until prices.size - 1) {
-        //      // buying here so implies negative - losing money
-        //      buy = -prices[i]
-        //      for (j in i + 1 until prices.size) {
-        //         // selling here implies positive - gains
-        //         sell = prices[j]
-        //         var temp = buy + sell
-        //         println("sell: $sell, buy: $buy profit $temp")
-        //         if (temp > 0) {
-        //             finalProfit = max(temp, finalProfit)
-        //         }
-        //      }
-        //  }
     }
 }
