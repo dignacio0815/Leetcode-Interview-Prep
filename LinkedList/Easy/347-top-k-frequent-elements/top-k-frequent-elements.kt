1ad60ad1d256615da224e584ab8b4223c4c39a15
Denize <41974125+dignacio0815@users.noreply.github.com>
2024-01-04 17:52:41 -0800

    Time: 288 ms (39.79%) | Memory: 40.7 MB (97.62%) - LeetSync

--- LinkedList/Easy/347-top-k-frequent-elements/top-k-frequent-elements.kt
+++ LinkedList/Easy/347-top-k-frequent-elements/top-k-frequent-elements.kt
@@ -1,20 +1,24 @@
 class Solution {
     fun topKFrequent(nums: IntArray, k: Int): IntArray {
-        val map = mutableMapOf<Int, Int>()
-        
+        // init map
+        val map = mutableMapOf<Int,Int>()
+        // init priority queue min heap
+        val pQueue = PriorityQueue<Int>( compareBy { map[it] } )
+
+        // creating frequency count for each num
         nums.forEach {
             map[it] = map.getOrDefault(it, 0) + 1
         }
 
-        // comparator here is creating a min heap
-        val pQueue = PriorityQueue<Int>( compareBy { map[it] } )    
-
-        for ((key, value) in map) {
+        map.forEach { key, _ ->
+            // inserting into heap
             pQueue.offer(key)
+            // if queue size > k, than remove pop current item in queue
+            // this works because its a min heap implying elements with lowest frequency
+            // are at start of queue
             if (pQueue.size > k) pQueue.poll()
         }
 
-        println(pQueue)
         return pQueue.toIntArray()
     }
 }
