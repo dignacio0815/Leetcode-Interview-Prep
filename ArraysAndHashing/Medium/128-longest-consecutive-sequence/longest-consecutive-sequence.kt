88b06cc8180b2399b04d3ece52f72a931e294810
Denize <41974125+dignacio0815@users.noreply.github.com>
2024-01-07 00:58:50 -0800

    Time: 1005 ms (23.61%) | Memory: 60.1 MB (33.13%) - LeetSync

--- ArraysAndHashing/Medium/128-longest-consecutive-sequence/longest-consecutive-sequence.kt
+++ ArraysAndHashing/Medium/128-longest-consecutive-sequence/longest-consecutive-sequence.kt
@@ -1,15 +1,20 @@
 class Solution {
     fun longestConsecutive(nums: IntArray): Int {
+        // init set
         val set = nums.toSet()
         var sequenceLength = 0
         
         nums.forEach { n ->
+            // if set doesn't contain the previous number, this implies the start of a new sequence
             if (!set.contains(n - 1)) {
+                // length to be added to n is 1 because a sequence is reprsented as [n, n + 1, n + 2, n + n ...]
                 var length = 1
                 // check if a sequence is contained in set atm
                 while (set.contains(length + n)) {
+                    // if the set contains this n + length number, than increment the length
                     length += 1
-                }
+                } 
+                // we only care about max length so when while loop is done either get the new max length or continue
                 sequenceLength = max(sequenceLength, length)
             }
         }
