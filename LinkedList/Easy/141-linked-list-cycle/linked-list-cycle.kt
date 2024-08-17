a4b2215f0be2dd0dc969fcdd17ddda6d1aab6794
Denize <41974125+dignacio0815@users.noreply.github.com>
2024-08-17 13:45:20 -0700

    Time: 223 ms (11.10%) | Memory: 38.2 MB (30.14%) - LeetSync

--- LinkedList/Easy/141-linked-list-cycle/linked-list-cycle.kt
+++ LinkedList/Easy/141-linked-list-cycle/linked-list-cycle.kt
@@ -10,7 +10,43 @@
 
 class Solution {
     fun hasCycle(head: ListNode?): Boolean {
+        return twoPointerApproach(head)
+    }
+
+    fun twoPointerApproach(head: ListNode?): Boolean {
         /*
+        keep 2 pointers pointed to head and head.next.next
+        the reason we do 2 nexts is because, if it was only going to be 1 next, than this would have solved example 2
+
+        for example,
+            slow pointer is head
+            fast pointer is head.next.next
+            in example 2, this would continuously loop since they never intersect
+            but with head.next.next eventually the 2 would intersect confirming a cycle
+
+        proof:
+            slow pointer at head
+            fast pointer at head.next.next
+            keep looping each to the next and eventually they should intersect
+            if they do, its a cycle
+            else reaches end of linked list, not a cycle
+         */
+         if (head == null) {return false}
+         var slow = head
+         var fast = head?.next?.next
+
+         while (slow != null) {
+            if (slow == fast) {
+                return true
+            }
+            slow = slow?.next
+            fast = fast?.next?.next
+         }
+         return false
+    }
+
+    fun setApproach(head: ListNode?): Boolean {
+                /*
             iterate through the linked list 
             everytime i see a node, add that to my set
             while looking through the linked list, I always check if the currrent node is in the set already
