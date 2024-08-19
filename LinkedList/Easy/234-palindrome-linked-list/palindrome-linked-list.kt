77ae2487b80c0df9c991786113cab32d95607fde
Denize <41974125+dignacio0815@users.noreply.github.com>
2024-08-19 16:20:09 -0700

    Time: 426 ms (83.45%) | Memory: 52.2 MB (70.77%) - LeetSync

--- LinkedList/Easy/234-palindrome-linked-list/palindrome-linked-list.kt
+++ LinkedList/Easy/234-palindrome-linked-list/palindrome-linked-list.kt
@@ -9,7 +9,51 @@
  */
 class Solution {
     fun isPalindrome(head: ListNode?): Boolean {
-        return bruteForce(head)
+        return optimal(head)
+        // return bruteForce(head)
+    }
+
+    fun optimal(head:ListNode?):Boolean {
+        /*
+            get the length of the linked list
+            loop until halfway point
+            if length is even, move midPtr by 1 
+            reverse remaining right part of linked list
+            take head and head of reversed linked list and compare only returning false if there's a mismatch
+         */
+
+        var length = 0
+        var ptr = head
+        while (ptr != null) {
+        length+=1
+        ptr = ptr?.next
+        }
+
+        ptr = head
+        
+        for (i in 0 until length - length.floorDiv(2)) {
+        ptr = ptr?.next
+        }
+
+        var prev:ListNode? = null
+        var cur:ListNode? = ptr
+        var next:ListNode? = cur?.next
+
+        while (cur != null) {
+        cur?.next = prev
+        prev = cur
+        cur = next
+        next = cur?.next
+        }
+
+        var h = head
+        while (prev != null) {
+        prev 
+        if (prev?.`val` != h?.`val`) {return false}
+        h = h?.next
+        prev = prev?.next
+        }
+        return true
     }
 
     fun bruteForce(head:ListNode?): Boolean {
