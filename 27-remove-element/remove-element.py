b34ed4d2e67b23e26e397fa52b8a89d698408fa8
dignacio0815 <41974125+dignacio0815@users.noreply.github.com>
2025-03-08 20:12:47 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (52.77%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,7 +1,6 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         i, j = 0, len(nums) - 1
-        ret = 0
         while (i <= j):
             # if i == val, go to j and find spot != val and swap, move up / down i and j respectively
             if (nums[i] == val):
@@ -12,18 +11,5 @@ class Solution:
                 elif (nums[j] == val):
                     j -= 1
             elif (nums[i] != val):
-                ret += 1
                 i+=1
-    
-        print(ret)
-        print(i)
-        print(nums)
-        return i  
-
-
-
-'''
-remove all numbers matching val in order
-keep track of all the element != to val
-k is number of elements not equal to val
-'''
+        return i
