e8d1e09766c03e23a5d43037cb042a6887ccef7c
dignacio0815 <41974125+dignacio0815@users.noreply.github.com>
2025-04-03 20:17:19 -0700

    Time: 96 ms (5.12%) | Memory: 20.9 MB (96.49%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        d = defaultdict(int)
+        l = []
+        for n in nums:
+            d[n] += 1
+
+        while k > 0:
+            maxVal = max(d.items(), key=lambda x: x[1])
+            l.append(maxVal[0])
+            d.pop(maxVal[0])
+            k -= 1
+        print(d)
+        print(l)
+        return l 
+"""
+given k, return k most frequent meaning give the numbers in the list that appear the most times but only the top "k" numbers
+
+so for example, if you had [1,2,3,3,4,4,4] k = 2 --> [3,4] 
+
+since 3 and 4 are the most frequent numbers followed by 1 and 2
+
+create a dict that loops through nums
+    counts occurences
+
+create a set to hold k frequent ints
+
+while k > 0:
+    grab max from dict store key
+    pop (key, value) from dict
+
+return list
+"""        
